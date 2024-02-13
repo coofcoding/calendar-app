cc4824424807ad1214082c0b6ea7244af58e9e0b
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-02-13 17:12:47 -0500

    styling Navbar component

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,6 +1,18 @@
+import { CalendarIcon, ArrowLeftStartOnRectangleIcon } from '@heroicons/react/24/solid'
 
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className='bg-violet-100 m-3 rounded-full p-4 flex justify-between items-center'>
+            <div className='flex items-center gap-2'>
+                <CalendarIcon className="ml-4 h-6 w-6 text-violet-500" />
+                <span className='text-violet-500 font-semibold'>myCalendary</span>
+            </div>
+            <button
+                type="button"
+                className="px-3 py-1.5 font-medium text-sm rounded-full bg-violet-700 hover:bg-violet-800 text-white focus:ring-2 focus:ring-violet-400  flex items-center gap-1">
+                <ArrowLeftStartOnRectangleIcon className="h-6 w-6 text-white" />
+                Logout
+            </button>
+        </div>
+    )
 }
